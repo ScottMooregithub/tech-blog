33486c2c272160aa53215d138ac128db6199120f
Scott Moore <sm91srjc@gmail.com>
2022-08-10 11:50:32 -0700

    routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,180 @@
+const router = require("express").Router();
+const sequelize = require("../../config/connection");
+const { post, User, Comment, Vote } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// get all users
+router.get("/", (req, res) => {
+  post
+    .findAll({
+      attributes: [
+        "id",
+        "post_text",
+        "post_img",
+        "title",
+        "created_at",
+
+        [
+          sequelize.literal(
+            "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+          ),
+          "vote_count",
+        ],
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  post
+    .findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: [
+        "id",
+        "post_text",
+        "post_img",
+        "title",
+        "created_at",
+
+        [
+          sequelize.literal(
+            "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+          ),
+          "vote_count",
+        ],
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", withAuth, (req, res) => {
+  post
+    .create({
+      // post_img: req.body.post_img,
+      title: req.body.title,
+      post_text: req.body.post_text,
+      user_id: req.session.user_id,
+    })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/upvote", withAuth, (req, res) => {
+  // custom static method created in models/post.js
+  post
+    .upvote(
+      { ...req.body, user_id: req.session.user_id },
+      { Vote, Comment, User }
+    )
+    .then((updatedVoteData) => res.json(updatedVoteData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", withAuth, (req, res) => {
+  post
+    .update(
+      {
+        title: req.body.title,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", withAuth, (req, res) => {
+  post
+    .destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
